9b71cab7be70d09e7fd516c4a4fa1c6f1ab3f339
Roberto Dias <roberto_rhd@hotmail.com>
2021-11-30 12:01:03 -0300

    refatoração

--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -1,10 +1,6 @@
-export default function reducer(state = [], action) { 
-    console.log('ADD_PRODUCT_TO_CART reducer')
+export default function reducer(state = [], action) {
+  if (action.type === "ADD_PRODUCT_TO_CART") 
+    return [...state, action.payload];
 
-    switch(action.type) {
-        case "ADD_PRODUCT_TO_CART":
-            return [...state, action.payload]
-        default: 
-            return state
-    }
+  return state;
 }
