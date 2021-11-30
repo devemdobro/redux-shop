4e50a8e6ec45c45e50d4d195492815c8c9d030ce
Roberto Dias <roberto_rhd@hotmail.com>
2021-11-30 12:06:11 -0300

    refatoração

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import { Provider } from "react-redux";
 import { Header } from "./components/header";
 import store from "./store/store";
 import { createGlobalStyle } from "styled-components";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./pages/home";
-import { CartProducts } from './pages/cart-products'
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./pages/routes";
 
 function App() {
   return (
@@ -13,10 +12,7 @@ function App() {
         <GlobalStyle />
         <BrowserRouter>
           <Header />
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/cart-products" element={<CartProducts />} />
-          </Routes>
+          <AppRoutes />
         </BrowserRouter>
       </Provider>
     </>
--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,16 +1,14 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import { Home } from "./home";
-// import { CartProducts } from './cart-products'
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./home";
+import { CartProducts } from "./cart-products";
 
-// const AppRoutes = () => {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route exact path="/" element={<Home />} />
-//         <Route exact path="/cart-products" element={<CartProducts />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// };
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/cart-products" element={<CartProducts />} />
+    </Routes>
+  );
+};
 
-// export { AppRoutes };
+export { AppRoutes };
